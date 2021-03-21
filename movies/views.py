367bc7c61d3d289a55befe80725b7820488b2301
georgehuble <nevajno23@mail.ru>
2021-03-21 11:34:11 +0300

    commit 2

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,21 @@
+from django.db import models
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from movies.models import Movie
 from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer, ReviewSerializer, \
     CreateRatingSerializer
+from .service import get_client_ip
 
 
 class MovieListView(APIView):
     """Вывод списка фильмов"""
     def get(self, request):
-        movies = Movie.objects.filter(draft=False)
+        movies = Movie.objects.filter(draft=False).annotate(
+            rating_user=models.Count('ratings', filters=models.Q(ratings__ip=get_client_ip(request)))
+        ).annotate(
+            middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
+        )
         serializer = MovieListSerializer(movies, many=True)
         return Response(serializer.data)
 
@@ -40,19 +46,10 @@ class ReviewView(APIView):
 
 class AddStarRatingView(APIView):
     """Добавление рейтинга к фильму"""
-
-    def get_client_ip(self, request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(', ')[0]
-        else:
-            ip = request.META.get('REMOTE_ADDR')
-        return ip
-
     def post(self, request):
         serializer = CreateRatingSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(ip=self.get_client_ip(request))
+            serializer.save(ip=get_client_ip(request))
             return Response(status=201)
         else:
             return Response(status=400)
